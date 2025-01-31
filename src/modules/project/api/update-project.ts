02604bcc7ac7ec4c21edd0cb79705306c69a24e1
Roman Filonenko <xsspiritttt@gmail.com>
2025-01-31 18:36:55 +0300

    Fix updateProject

--- src/modules/project/api/update-project.ts
+++ src/modules/project/api/update-project.ts
@@ -7,21 +7,21 @@ import { revalidatePath } from "next/cache";
 export const updateProject = async (project: UpdateProjectRequestSchema) => {
   try {
     const supabase = await createServerSupabase();
-    project.id = "";
     const { data } = await supabase
       .from("projects")
       .update(project)
       .eq("id", project.id)
       .select("*")
+      .single()
       .throwOnError();
 
-    revalidatePath("/", "layout");
     return projectSchema.parse(data);
   } catch (error) {
-    revalidatePath("/", "layout");
     if (error instanceof Error) {
       error.cause = { nextNoDigest: true, originalCause: error.cause };
       throw error;
     }
+  } finally {
+    revalidatePath("/", "layout");
   }
 };
