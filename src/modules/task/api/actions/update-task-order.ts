59be9a2ae76053b80e1062c6c791c5a30258211c
Roman Filonenko <xsspiritttt@gmail.com>
2025-02-11 01:48:59 +0300

    make update task order ussual function

--- src/modules/task/api/actions/update-task-order.ts
+++ src/modules/task/api/actions/update-task-order.ts
@@ -1,20 +1,9 @@
-"use server";
-
-import { createServerSupabase } from "@/lib/supabase/create-server-supabase";
+import { supabase } from "@/lib/supabase/create-browser-supabase";
 import { Tasks, tasksSchema } from "@/schemas/task-schema";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
 
 export const updateTaskOrder = async (updatedTasks: Tasks) => {
   try {
     const validatedData = tasksSchema.parse(updatedTasks);
-
-    const supabase = await createServerSupabase();
-
-    const user = await supabase.auth.getUser();
-    if (!user) {
-      redirect("/auth/login");
-    }
 
     const { data } = await supabase
       .from("tasks")
@@ -25,7 +14,5 @@ export const updateTaskOrder = async (updatedTasks: Tasks) => {
     return tasksSchema.parse(data);
   } catch (error) {
     if (error instanceof Error) return error;
-  } finally {
-    revalidatePath("/", "layout");
   }
 };
