0d4215fe226d4e2d5d7cfd8ef5dce00caf89f9ed
Roman Filonenko <xsspiritttt@gmail.com>
2025-02-10 22:47:01 +0300

    test

--- src/app/app/today/page.tsx
+++ src/app/app/today/page.tsx
@@ -1,21 +1,32 @@
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { createServerSupabase } from "@/lib/supabase/create-server-supabase";
-import { taskApi } from "@/modules/task/api/task-api";
-import { makeQueryClient } from "@/lib/get-query-client";
-import TodayPage from "./client-page";
+"use client";
 
-export default async function Today() {
-  const supabase = await createServerSupabase();
+import { CreateTask } from "@/modules/task/components/create-task";
+import { Task } from "@/modules/task/components/task";
+import { useCreateTask } from "@/modules/task/hooks/use-create-task";
+import { useQueryPageTasks } from "@/modules/task/hooks/use-query-page-tasks";
+import { startOfDay } from "date-fns";
 
-  const queryClient = makeQueryClient();
+export default function TodayPage() {
+  const { mutate: handleCreate } = useCreateTask("today");
 
-  await Promise.allSettled([
-    queryClient.fetchQuery(taskApi.getTodayTasksQueryOptions(supabase)),
-  ]);
+  const { tasks } = useQueryPageTasks();
+
+  const defaultDate = startOfDay(new Date());
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <TodayPage />
-    </HydrationBoundary>
+    <div className="flex flex-col flex-auto max-w-[85vw] lg:max-w-3xl">
+      <h1 className="mb-4">Today</h1>
+      <div className="flex flex-col">
+        {tasks?.map((task) => (
+          <Task param="today" isSortable={false} key={task.id} {...task} />
+        ))}
+        <CreateTask
+          projectId={null}
+          createTask={handleCreate}
+          order={null}
+          defaultDate={defaultDate}
+        />
+      </div>
+    </div>
   );
 }
