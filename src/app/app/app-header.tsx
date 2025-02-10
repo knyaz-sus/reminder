eb48a2462e9c0d3cdacba3ecabb153c38a694c49
Roman Filonenko <xsspiritttt@gmail.com>
2025-02-11 01:30:04 +0300

    test

--- src/app/app/app-header.tsx
+++ src/app/app/app-header.tsx
@@ -5,11 +5,11 @@ import { ViewOptions } from "./view-options";
 import { useSidebar } from "@/hooks/use-sidebar";
 
 export function AppHeader() {
-  const { isOpen } = useSidebar();
+  const { isOpen, isMobile } = useSidebar();
 
   return (
     <header className="flex fixed w-full justify-between h-[52px] z-[2] bg-background">
-      {!isOpen && <SidebarTrigger />}
+      {(!isOpen || isMobile) && <SidebarTrigger />}
       <div className="flex gap-4 items-center absolute right-6 top-3">
         <ViewOptions />
       </div>
